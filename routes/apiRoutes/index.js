5dd27b1eeaf5c024b95d891b7dddc37e68e743c3
Junioresc <junioresc1092@gmail.com>
2020-08-16 00:34:34 -0400

    added a unique id to each note and add fs functionality to the json file

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,21 @@
 const path = require(`path`);
 const router = require(`express`).Router();
+const fs = require(`fs`);
+const uniqid = require(`uniqid`);
 let { notes } = require(`../../db/db.json`);
 
 router.get(`/notes`, (req, res) => {
-    let results = notes;
-    res.json(results);
+    res.json(notes);
 });
 
 router.post(`/notes`, (req, res) => {
-    req.body.id = notes.length.toString();
+    req.body.id = uniqid();
 
     const note = req.body;
     notes.push(note);
+    fs.writeFileSync(path.join(__dirname, `../../db/db.json`),
+     JSON.stringify({ notes: notes }, null, 2)
+     );
     res.json(note);
 });
 
@@ -20,6 +24,9 @@ router.delete(`/notes/:id`, (req, res) => {
     const deleted = notes.find(note => note.id === id);
     if(deleted) {
         notes = notes.filter(note => note.id !== id);
+        fs.writeFileSync(path.join(__dirname, `../../db/db.json`),
+         JSON.stringify({ notes: notes }, null, 2)
+         );
         res.status(200).json(deleted);
     } else {
         res.send(404);
